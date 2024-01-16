acca9bcc35a9aeb56048a8f9945d9fa1d1b50b13
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-17 00:00:04 +0300

    Return the User instance if all conditions are met

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic authentication for API
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +57,25 @@ class BasicAuth(Auth):
         user_email, user_pass = decoded_base64_authorization_header.split(
                 ':', 1)
         return user_email, user_pass
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Gets User instance based on email and password
+        """
+        if user_email is None or not isinstance(
+                user_email,
+                str) or user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+
+        if not users:
+            return None
+
+        user_instance = users[0] if isinstance(users, list) else users
+
+        if not user_instance.is_valid_password(user_pwd):
+            return None
+
+        return user_instance
