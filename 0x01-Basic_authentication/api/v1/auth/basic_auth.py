b7801c4d2f71bc717e58c71f275214dc66e315f8
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-16 23:19:13 +0300

    Retriving the user email and password from the Base64 decoded value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,19 @@ class BasicAuth(Auth):
             return decoded_value
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        extracts Basic - User credentials
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_pass = decoded_base64_authorization_header.split(
+                ':', 1)
+        return user_email, user_pass
