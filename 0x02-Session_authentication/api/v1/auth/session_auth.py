12523c7309af8b8db7ce4fc1a539b8265b7c96b2
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-19 00:15:10 +0300

    Creating Session/IDs for the user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,30 @@
 SessionAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session-based Authentication Class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id
+
+        Args:
+            user_id : user ID for which a session is to be created
+
+        Returns:
+            str: generated Session ID, otherwise None
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
