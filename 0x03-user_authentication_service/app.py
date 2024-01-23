e33790a835de7c3ea6cd8eb187fcb263c201badd
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-23 21:37:22 +0300

    Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """
+    return a JSON message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({'email': user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
