3ad63632bf7dbb94713e0b43b272e7caffa442f8
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-24 16:18:43 +0300

    Added a method that retrieves user based on the provided session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,3 +104,22 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User or None:
+        """
+        Gets user based on the provided session ID
+
+        Args:
+            session_id: The session ID
+
+        Returns:
+            User or None: the User if found else None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id)
+            return user
+        except NoResultFound:
+            return None
