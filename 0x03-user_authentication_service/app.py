1f64541e9bca7a5de8e9da6ce26941ca60c2df88
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-24 22:58:57 +0300

    If either condition is not met (i.e., if user is None or session_id is None), it responds with a 403 status.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,7 +57,7 @@ def logout() -> str:
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
-    if user is not None and session_id is not None:
+    if user is not None or session_id is not None:
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
