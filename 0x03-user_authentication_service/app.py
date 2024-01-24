3f28176e584a21fbbb75f6e57870a0d1776c3468
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-24 23:22:44 +0300

    Fixing to correct output

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,7 +57,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
-    if user is not None:
+    if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
