64b5cf0c2d14bf202787934ec8edb7e1399ac072
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-25 00:48:51 +0300

    This added method Generates a reset password token for the user based on the provided email

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,5 +81,22 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Generates reset password token for user of the givev email
+
+    Returns:
+        JSON: Reset password token or 403 status if email is not
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        response = {"email": email, "reset_token": reset_token}
+        return jsonify(response), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
