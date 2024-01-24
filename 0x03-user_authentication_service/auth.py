5a148e32c5aa176d7723af22ee3c84a94b282db9
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-25 00:08:05 +0300

    generate a UUID and update the user’s reset_token database field. Return the token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -139,3 +139,23 @@ class Auth:
             return self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Gets the reset password token for user the given email
+
+        Args:
+            email: email of the user.
+
+        Returns:
+            str: reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+
+        return reset_token
